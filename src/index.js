780d7c31e8e1d85c404e8d0738e6394f65119cd1
Surya Duggirala <suryad96@berkeley.edu>
2017-05-30 16:36:35 -0700

    all but done. I just need to finish deciding who the winner is

--- src/index.js
+++ src/index.js
@@ -1,31 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
+/*class Square extends React.Component {
+  /* THE DEFAULT VALUE OF THE SQUARE WILL BE NULL WHEN WE FIRST CREATE IT. 
+    IT EVENTUALLY TURNS INTO AN 'X' ON FIRST CLICK AS PER THE EVENT HANDLER WE REPLACE
+    THE ALERT WITH. /
+  constructor() {
+    super();
+    this.state = {
+      value: 'null',
+    };
+  }
   render() {
     /* THIS WILL RETURN A BOX WITH NUMBERED VALUES BECAUSE WE ARE 
-     CALLING renderSquare(i) IN RANGE(1, 7) FROM THE GAME CLASS. */
+     CALLING renderSquare(i) IN RANGE(1, 7) FROM THE GAME CLASS. /
     // ALSO AS A SIDE NOTE, DON'T PLACE COMMENTS INSIDE RETURN STATEMENT. IT THROWS RENDER OFF.
+
+    // JAVASCRIPT ALSO COMPILES IN REAL TIME SO AS SOON AS I CHANGE SOMETHING IN MY CODE,
+    // THE COMPILER AUTOMATICALLY STARTS COMPILING THE CODE AND EITHER GIVES YOU AN ERROR MESSAGE OR A 
+    // FINISHED PRODUCT.
+
+    // #### ADDED CODE ####
     return (
-      <button className="square">
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
   }
+}*/
+// SINCE WE ONLY HAVE A RENDER METHOD, IT'S SIMPLER TO JUST MAKE THIS A FUNCTION
+function Square(props) {
+  return <button className="square"
+   onClick={props.onClick}>
+   {props.value}
+   </button> 
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // POPULATE AN ARRAY THAT WILL REPRESENT ALL MY SQUARES.
+      // COLON NOTATION IMPLIES WE'RE WORKING WITH STATE. WE'RE DEFINING STATE SO 
+      // NO '='
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
   renderSquare(i) {
     /* THIS ALLOWS ME TO CREATE A SQUARE COMPONENT WITH A VALUE OF i.
      REACT WORKS WITHIN HTML BY CREATING COMPONENTS (WHICH ARE COMPLETELY MODULARIZED) 
      THAT GO INSIDE THE ACTUAL HTML. ESSENTIALLY, WE CAN BUILD OUT WEBSITES BY CREATING 
      'PARTS' SIMILAR TO CSS CLASSES EXCEPT WITH MORE DYNAMIC FUNCTIONALITIES.
     */
-    return <Square value={i}/>;
+    // #### ADDED CODE ####
+    // THIS IS GOING TO RETURN THE I'TH SQUARE IN MY ARRAY.
+    return ( <Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i) {
+    // CREATING CLONE OF THE SQUARES ARRAY. OBJECT POINTERS REMAIN THE SAME THOUGH.
+    // [|, |, |, |] --- STATE ARRAY
+    // [A, B, C, D] --- OBJECTS IN MEMORY
+    // [|, |, |, |] --- NEW ARRAY IN HANDLECLICK()
+    const newSquares = this.state.squares.slice();
+    // WHY DOESN'T THIS WORK? 
+    //
+    // if (this.xIsNext) {
+    //   newSquares[i] = 'X';
+    // } else {
+    //     newSquares[i] = 'O';
+    // }
+    newSquares[i] = this.state.xIsNext ? 'X' : 'O';
+    // FACEBOOK USES A DIFFERENT SYNTAX: 
+    // SQUARES[I] = THIS.STATE.XISNEXT ? 'X' : 'O';
+    // this.xIsNext = !this.xIsNext;
+    // SET THE STATE OF THE TIC TAC TOE BOARD AS THE NEWSQUARES
+    this.setState({squares: newSquares,
+      xIsNext: !this.state.xIsNext});
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
